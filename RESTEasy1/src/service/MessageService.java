3c6a889e0dbcf1902482e036e59f1d6bb62e04eb
Sefa Üncü <sefauncu@gmail.com>
2018-06-05 16:07:24 +0300

    source code

--- RESTEasy1/src/service/MessageService.java
+++ RESTEasy1/src/service/MessageService.java
@@ -9,7 +9,7 @@ public class MessageService {
 	@GET
 	@Path("/greeting")
 	
-	// http://localhost:8080/RESTEasyWeek01/messages/greeting
+	//http://localhost:8080/RESTEasyWeek01/messages/greeting
 	public String getMessage() {
 		return "Hello RESTEasy world!!";
 	}
